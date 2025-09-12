e179ba5cd33bd6a381bd729364e2b463c7c7fec8
karpritam <karpritam194@gmail.com>
2025-09-12 23:49:38 +0530

    complete menubar

--- Billify-frontend/src/components/MenuBar/MenuBar.jsx
+++ Billify-frontend/src/components/MenuBar/MenuBar.jsx
@@ -25,8 +25,8 @@ const MenuBar = () => {
 	};
 	return (
 		<div>
-			<nav className="absolute top-0 left-0 w-full z-10 bg-gray-900">
-				<div className="container mx-auto flex justify-between items-center py-4 px-6 md:px-20 lg:px-32 ">
+			<nav className="flex justify-between absolute top-0 left-0 w-full z-10 bg-gray-900">
+				<div className="container mx-auto flex justify-start gap-7 items-center py-4 px-6 md:px-20 lg:px-32 ">
 					<a href="#">
 						<img
 							className="cursor-pointer h-10 w-auto"
@@ -35,74 +35,82 @@ const MenuBar = () => {
 						/>
 					</a>
 					<ul className="hidden md:flex gap-7 text-white">
-						<a href="#" className="cursor-pointer hover:text-green-500">
+						<a
+							href="#DASHBOARD"
+							className="cursor-pointer hover:text-green-500">
 							DASHBOARD
 						</a>
-						<a href="#" className="cursor-pointer hover:text-green-500">
+						<a href="#EXPLORE" className="cursor-pointer hover:text-green-500">
 							EXPLORE
 						</a>
-						<a href="#" className="cursor-pointer hover:text-green-500">
+						<a
+							href="#MANAGE ITEMS"
+							className="cursor-pointer hover:text-green-500">
 							MANAGE ITEMS
 						</a>
-						<a href="#" className="cursor-pointer hover:text-green-500">
+						<a
+							href="#MANAGE CATEGORIES"
+							className="cursor-pointer hover:text-green-500">
 							MANAGE CATEGORIES
 						</a>
-						<a href="#" className="cursor-pointer hover:text-green-500">
+						<a
+							href="#MANAGE USERS"
+							className="cursor-pointer hover:text-green-500">
 							MANAGE USERS
 						</a>
 					</ul>
-					<button className="hidden md:block px-8 py-2 rounded-full">
-						Sign Up/Login
-					</button>
-					<img
-						onClick={handleMenuOpen}
-						src={assets.menuIcon}
-						alt="menu"
-						className="md:hidden w-7 cursor-pointer"
-					/>
-                    
 				</div>
+				<button className="hidden md:block px-10 py-2 rounded-full text-white">
+					Sign Up/Login
+				</button>
+				<button
+					onClick={handleMenuOpen}
+					className="md:hidden cursor-pointer pr-10"
+					aria-label="Open menu"
+					aria-expanded={showMobileMenu}>
+					<img src={assets.menuIcon} alt="" className="w-7" />
+				</button>
 			</nav>
 			{/* -----------mobile menu------------- */}
 			<nav
 				className={`md:hidden ${
-					showMobileMenu ? "fixed w-[30%]" : "h-0 w-0"
-				} right-0 top-0 bottom-0 rounded-md overflow-hidden bg-green-200 transition-all duration-500 ease-in-out`}>
+					showMobileMenu ? "fixed w-[40%]" : "h-0 w-0"
+				} right-0 top-0 bottom-0 rounded-md overflow-hidden bg-green-100 transition-all duration-500 ease-in-out z-20`}>
 				<div className="flex justify-start p-6 cursor-pointer">
 					<img
 						onClick={handleMenuClose}
 						src={assets.crossIcon}
-						className="p-2 w-10 cursor-pointer hover:bg-green-500 rounded transition-transform duration-300"
+						className="p-2 w-10 cursor-pointer hover:bg-green-500 rounded "
 					/>
 				</div>
 				<ul className="flex flex-col items-start gap-2 mt-5 px-5 text-lg font-medium">
 					<a
 						onClick={() => setShowMobileMenu(false)}
-						href="#"
+						href="#DASHBOARD"
 						className="px-4 py-2 rounded-full inline-block">
 						DASHBOARD
 					</a>
 					<a
 						onClick={() => setShowMobileMenu(false)}
-						href="#"
+						href="#EXPLORE"
 						className="px-4 py-2 rounded-full inline-block">
 						EXPLORE
 					</a>
 					<a
 						onClick={() => setShowMobileMenu(false)}
-						href="#"
+						href="#MANAGE ITEMS"
 						className="px-4 py-2 rounded-full inline-block">
 						MANAGE ITEMS
 					</a>
 					<a
 						onClick={() => setShowMobileMenu(false)}
-						href="#"
+						href="#MANAGE CATEGORIES"
 						className="px-4 py-2 rounded-full inline-block">
 						MANAGE CATEGORIES
 					</a>
 					<a
 						onClick={() => setShowMobileMenu(false)}
-						href="#"
+						href="#MANAGE USERS"
 						className="px-4 py-2 rounded-full inline-block">
 						MANAGE USERS
 					</a>
